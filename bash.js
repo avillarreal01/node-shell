30bd1ecce60a56015216f871537b50dbef344c5a
Samuel Yun <shysam.yun@gmail.com>
2017-09-18 09:30:56 -0400

    first step

--- bash.js
+++ bash.js
@@ -1,2 +1,8 @@
-'Hello Alex'
-'hey sam'
+process.stdout.write('prompt > ');
+
+process.stdin.on('data', function (data) {
+    var cmd = data.toString().trim();
+
+    process.stdout.write('You typed: ' + cmd);
+    process.stdout.write('\nprompt > ');
+});
