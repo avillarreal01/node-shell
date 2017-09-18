f7a8675ac794b704fb078195aa2e473bf027f873
Samuel Yun <shysam.yun@gmail.com>
2017-09-18 09:54:50 -0400

    uptp nodemon

--- bash.js
+++ bash.js
@@ -2,7 +2,15 @@ process.stdout.write('prompt > ');
 
 process.stdin.on('data', function (data) {
     var cmd = data.toString().trim();
-
-    process.stdout.write('You typed: ' + cmd);
+    if (cmd === 'pwd') {
+        process.stdout.write(process.cwd());
+    } else if (cmd === 'date') {
+        var date = new Date();
+        console.log(date.toUTCString());
+        // process.stdout.write(date);
+    } else {
+        process.stdout.write('You typed: ' + cmd);
+    }
+   
     process.stdout.write('\nprompt > ');
 });
