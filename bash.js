1a82f767200a20f65b2e4aba35e7760ceeb865a5
Samuel Yun <shysam.yun@gmail.com>
2017-09-18 12:22:09 -0400

    started piping

--- bash.js
+++ bash.js
@@ -8,7 +8,8 @@ var done = function(output) {
 process.stdout.write('prompt > ');
 
 process.stdin.on('data', function (data) {
-    var inputArr = data.toString().trim().split(' ');
+var inputArr = data.toString().trim().split(/\s|\|/);
+    console.log(inputArr);
     var cmd = inputArr[0];
     var arg = inputArr.slice(1).join(' ');
     if (commands[cmd]) {
